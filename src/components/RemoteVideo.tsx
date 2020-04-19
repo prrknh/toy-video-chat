c380d289d99e529aaf4e3b7420bf56d2d276bfbc
prrknh <prrknh@gmail.com>
2020-04-19 16:45:47 +0900

    fix

--- src/components/RemoteVideo.tsx
+++ src/components/RemoteVideo.tsx
@@ -1,20 +1,17 @@
-import React, { RefObject, useEffect } from 'react'
+import React, { RefObject } from 'react'
 // import { UseRoom } from './UseRoom'
 
 const RemoteVideo = (ref: RefObject<HTMLVideoElement>, stream: MediaStream) => {
   // const [, remoteStreamList] = UseRoom()
   // const streamRef = useRef<HTMLVideoElement>(null)
-  useEffect(() => {
-    // const currentRemoteStream = remoteStreamList[index]
-    if (!ref || !ref.current) {
-      console.log('nothing')
-      return
-    }
-    console.log(ref.current)
-    ref.current.srcObject = stream
-    console.log(ref.current.srcObject)
+  // const currentRemoteStream = remoteStreamList[index]
+  if (!ref || !ref.current) {
+    console.log('nothing')
     return
-  })
+  }
+  console.log(ref.current)
+  ref.current.srcObject = stream
+  console.log(ref.current.srcObject)
 
   return <video ref={ref} id="hoge" />
 }
